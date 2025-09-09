eb3acbf560cce657f8f5863ca3058829eb87945a
neoarmstrong99 <noguken.doctor@gmail.com>
2025-09-10 01:15:49 +0900

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useRouter, useSegments } from 'expo-router';
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
   useFrameworkReady();
